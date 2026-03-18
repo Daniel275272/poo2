8815919aaa62f1955e44febb1b582ea79359ed6d
Daniel275272 <daniel.barahona@campusucc.edu.co>
2026-03-18 01:25:06 +0000

    perro  y gato

--- semana7/Animal.java
+++ semana7/Animal.java
@@ -1,5 +1,37 @@
 package semana7;
 
 public class Animal {
-    
+     private String nombre;
+    private int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() { 
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void comer() {
+        System.out.println(nombre + " está comiendo.");
+    }
+
+    public void hacerSonido() {
+        System.out.println(nombre + " hace un sonido genérico.");
+    }
+
+    public String toString() {
+        return "Animal[ nombre=" + nombre + ", Edad=" + edad + "]";
+        
+        
+    }
 }
---
+++ semana7/Perro.java
@@ -0,0 +1,32 @@
+package semana7;
+
+public class Perro extends Animal {
+    private String raza;
+
+    public Perro(String nombre, int edad, String raza) {
+        super(nombre, edad);
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(getNombre() + " dice: ¡Guau!");
+    }
+
+    public void buscarPelota() {
+        System.out.println(getNombre() + " fue a buscar la pelota.");
+    }
+    
+    @Override
+    public String toString() {
+        return "Perro[ " + super.toString() + ", Raza=" + raza + "]";
+    }
+}
